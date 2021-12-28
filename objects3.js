2dee1ba04b0664e61b09a1d8094952f6235df0e0
Taiwo Oluwaseun <taiwoluwaseun8@gmail.com>
2021-12-28 23:04:25 +0100

    class constructors

--- objects3.js
+++ objects3.js
@@ -1,4 +1,19 @@
-// Classes
+// Classes and Class Constructors
 class User {
-    
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
 }
+
+var userOne = new User('seun@gmail.com', 'Seun');
+var userTwo = new User('seyi@gmail.com', 'Seyi');
+
+console.log(userOne);
+console.log(userTwo);
+
+
+// the 'new' keyword
+// - creates a new empty object {}
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor method
